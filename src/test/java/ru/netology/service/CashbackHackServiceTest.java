bc943c4d800d9db2c49271d964a04b80803fd64d
taranchella <taranchella@icloud.com>
2024-02-17 19:54:30 +0300

    three

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,28 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ifAmount900() {
         assertEquals(service.remain(900), 100);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ifAmount0() {
         assertEquals(service.remain(1), 999);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ifAmount5000() {
         assertEquals(service.remain(10000), 1000);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ifAmount1000() {
         assertEquals(service.remain(1000), 0);
     }
